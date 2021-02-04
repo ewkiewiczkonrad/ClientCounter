634af28019dcbff1f91e78edbfd5b55678ba3a94
Dejas <wdeja05@gmail.com>
2021-02-04 16:46:48 +0100

    Nowa funkcjonalność - buttony : Dodawanie, Odejmowanie

--- app/src/main/java/com/example/clientcounter/CountActivity.java
+++ app/src/main/java/com/example/clientcounter/CountActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CountActivity extends AppCompatActivity {
 
@@ -22,7 +25,23 @@ public class CountActivity extends AppCompatActivity {
         people= intent.getIntExtra("licz",0);
 
         countText.setText(now+"/"+people);
-
-
     }
+
+    public void plusButtonClick(View v) {
+        if(now<people) {
+            now++;
+            countText.setText(now + "/" + people);
+        }else{
+            Toast.makeText(getApplicationContext(),"Limit osiągnięty",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void minusButtonClick(View v) {
+        if(now>0) {
+            now--;
+            countText.setText(now + "/" + people);
+        }
+    }
+
+
 }
