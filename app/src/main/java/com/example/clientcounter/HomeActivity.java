f9ace77c79e4599a33b00413e0e4857e5413e6d7
Dejas <wdeja05@gmail.com>
2021-02-09 18:45:10 +0100

    Added SettingActivity and working Buttons to HomeAcitivity

--- app/src/main/java/com/example/clientcounter/HomeActivity.java
+++ app/src/main/java/com/example/clientcounter/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.clientcounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void startButton(View v){
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void guestButton(View v){
+        Intent intent = new Intent(HomeActivity.this, MenuActivity.class);
+        startActivity(intent);
+    }
+
+    public void optionsButton(View v){
+        Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
